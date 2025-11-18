a90391113280966f23bd7c3577d5bb483423807a
Rajib Mahato <50389399+Ablindbat@users.noreply.github.com>
2025-11-18 21:31:33 +0530

    updated

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ const span = document.querySelector(".close");
 btn.onclick = () => { modal.style.display = "flex"; }
 span.onclick = () => { modal.style.display = "none"; }
 window.onclick = (e) => { if(e.target == modal) modal.style.display = "none"; }
+
+
+//New change
+
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+
+    if (mobileMenu.style.display === "flex") {
+        mobileMenu.style.display = "none";
+    } else {
+        mobileMenu.style.display = "flex";
+    }
+});
+
+// Close when clicking mobile menu link
+document.querySelectorAll(".mobile-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        mobileMenu.style.display = "none";
+    });
+});
+
